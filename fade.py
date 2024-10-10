3ead9eba7f0265dba6081cb1c5377db449ae795d
marcuskamryn <2025marcus.kamryn@fcboe.org>
2024-10-10 13:45:00 -0400

    Update fade.py
    fade in and fade out finial-ish code

--- fade.py
+++ fade.py
@@ -6,19 +6,48 @@ import random
 
 #setup
 np = NeoPixel(board.D2, 30, auto_write = False, brightness = 0.3)
-steps = 0
+steps = 50
 
-def fade_out(r, g, b):
-    if r > g and b:
-        steps = r
-    elif g > r and b:
-        steps = g
-    else:
-        steps = b
-    for i in range(steps):
-        np
+
+def fade_out(color, speed):
+    red_value = color[0]
+    green_value = color[1]
+    blue_value = color[2]
     
-def fade_in(r, g, b):
+    red_ratio = color[0] / 50
+    green_ratio = color[1] / 50
+    blue_ratio = color[2] / 50
+    
+    for i in range(51):
+        red = red_value - i*red_ratio
+        green = green_value - i*green_ratio
+        blue = blue_value - i*blue_ratio
+        np.fill((red, green, blue))
+        np.show()
+        time.sleep(speed)
+
+    
+def fade_in(color, speed):
+    red_value = color[0]
+    green_value = color[1]
+    blue_value = color[2]
+    
+    red_ratio = color[0] / 50
+    green_ratio = color[1] / 50
+    blue_ratio = color[2] / 50
+    
+    for i in range(51):
+        red = i*red_ratio
+        green = i*green_ratio
+        blue = i*blue_ratio
+        np.fill((red, green, blue))
+        np.show()
+        time.sleep(speed)
+        
+
+while True:
+    fade_out((255, 53, 78), 0.05)
+    fade_in((78, 53, 255), 0.05)
 
 """
 def fade_out():
