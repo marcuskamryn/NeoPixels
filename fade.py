f57633e19c1b103415777f145dc0dc0e9e224839
marcuskamryn <2025marcus.kamryn@fcboe.org>
2024-10-11 13:44:52 -0400

    Update fade.py
    Making a default variable

--- fade.py
+++ fade.py
@@ -9,7 +9,7 @@ np = NeoPixel(board.D2, 30, auto_write = False, brightness = 0.3)
 steps = 50
 
 
-def fade_out(color, speed):
+def fade_out(color, speed = 0.005):
     red_value = color[0]
     green_value = color[1]
     blue_value = color[2]
@@ -27,7 +27,7 @@ def fade_out(color, speed):
         time.sleep(speed)
 
     
-def fade_in(color, speed):
+def fade_in(color, speed = 0.005):
     red_value = color[0]
     green_value = color[1]
     blue_value = color[2]
@@ -48,39 +48,3 @@ def fade_in(color, speed):
 while True:
     fade_out((255, 53, 78), 0.05)
     fade_in((78, 53, 255), 0.05)
-
-"""
-def fade_out():
-    for i in range(256):
-        np.fill((0, 0, 255 - i))
-        np.show()
-
-def fade_in():
-    for i in range(256):
-        np.fill((0, 0, i))
-        np.show()
-    
-while True:
-    fade_in()
-    fade_out()
-"""
-
-
-"""
-i = 0
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-cyan = (0, 255, 255)
-magenta = (255, 0, 255)
-color_list = [red, green, blue, yellow, cyan, magenta]
-
-
-#main
-while True:
-    for i in range(np.n):
-        np[i] = random.choice(color_list)
-    np.show()
-    time.sleep(.1)
-"""
