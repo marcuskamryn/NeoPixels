1edcdcd1d6523d781669e2ddc838a9239b69f068
marcuskamryn <2025marcus.kamryn@fcboe.org>
2024-10-23 13:10:05 -0400

    Update chase.py

--- chase.py
+++ chase.py
@@ -7,13 +7,14 @@ np = NeoPixel(board.D2, 30, auto_write = False, brightness = 0.3)
 
 def chase(color):
     count = 0
-    for i in range(10):
-        np.fill((0, 0, 255))
+    for i in range(np.n):
+        np.fill(color)
         for i in range(np.n):
-            if(i + count % 3) == 0:
+            if(i + count) % 3 == 0:
                 np[i] = (0, 0, 0)
         np.show()
         time.sleep(0.1)
+        count = (count + 1) % 3
         
 while True:
     chase((0,0,255))
