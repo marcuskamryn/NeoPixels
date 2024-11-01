2a203bf22352227b4ab9f2366fd267d88076f406
marcuskamryn <2025marcus.kamryn@fcboe.org>
2024-11-01 13:25:07 -0400

    Update larson.py

--- larson.py
+++ larson.py
@@ -6,14 +6,35 @@ np = NeoPixel(board.D2, 30, auto_write = False, brightness = 0.3)
 
 def bounce(background, foreground, num):
     direction = 1
-    for i in range(num):
-        for i in range(np.n):
-            np.fill(background)
-            np[i * direction] = foreground
-            time.sleep(0.1)
-            np.show()
-        direction = direction * (-1)
+    i = 0
+    bc = 0
+    dim =  [int(c * 0.8) for c in foreground]
+    dimmer = [int(c * 0.2) for c in foreground]
+    
+    
+    while bc < num:
+        np.fill(background)
+        np[i] = foreground
+        if( i - 2 >= 0):
+            np[i - 2] = dimmer
+            
+        if(i - 1 >= 0):
+            np[i - 1] = dim
+            
+        if(i + 2 <= np.n - 1):
+            np[i + 2] = dimmer
+            
+        if(i + 1 <= np.n - 1):
+            np[i +1] = dim
+        
+        time.sleep(0.1)
+        np.show()
+        
+        i += direction
+        if(i >= (np.n - 1) or i <= 0):
+            direction *= -1
+            bc += 1
     
 
 while True:
-    bounce((0, 0, 255), (255, 0, 0), 4)
+    bounce((0, 0, 0), (255, 0, 0), 4)
